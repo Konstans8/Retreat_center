306ef530532a53e4fb04093a1cb6cd17bc0d08b5
konstans8 <kostian73kostian@gmail.com>
2023-01-22 23:33:33 +0100

    four commit

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -9,6 +9,12 @@ function Contacts() {
                     <h1 className={s.title}>Kontakty</h1>
                     <div className={s.info}>
                         <div className={s.data}>
+                            <div className={s.address}>
+                                <p>DANE ADRESOWE:</p>
+                                <p>Dom Rekolekcyjny Zgromadzenia Ksiezy Marianow</p>
+                                <p>05-070 Sulejówek </p>
+                                <p>ul. Mariańska 9</p>
+                            </div>
                             <p className={s.mail}>Mail:</p>
                             <a href="mailto:rekolekcje-sulejowek@marianie.pl">rekolekcje-sulejowek@marianie.pl</a>
                             <p className={s.tel}>Telefon:</p>
